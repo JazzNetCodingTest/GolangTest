d422e932cf1f004d99affc7446f76182b7b1228b
premdass <premdass.r@gmail.com>
2020-08-06 15:51:45 +0100

    Adding Array Intersection

---
+++ pkg/intersection.go
@@ -0,0 +1,26 @@
+package main
+
+
+func intersection(x,y []int) (z []int) {
+	lookup := make(map[int]bool)
+	
+	//convert one of the array in to map for easier lookup
+	for _,i := range x {
+		lookup[i] = false
+	}
+
+	// mark the element as true if it matches with other array
+	for _,i := range y {
+		if _, ok := lookup[i]; ok {
+			lookup[i] = true
+		}
+	}
+	//filter out all the matched elements
+	for key,i := range lookup {
+		if i {
+			z = append(z,key)
+		}
+	}
+	return 
+	
+}
---
+++ pkg/intersection_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+	"sort"
+)
+
+func TestIntersectionForBothEmptyArrays(t *testing.T) {
+	var a = []int{}
+	var b = []int{}
+	got := intersection(a, b)
+	var want []int
+	assertArrayEqual(got, want, t)
+}
+
+
+func TestIntersectionForBothOneEmptyArray(t *testing.T) {
+	var a = []int{1}
+	var b = []int{}
+	got := intersection(a, b)
+	var want []int
+	assertArrayEqual(got, want, t)
+}
+
+
+func TestIntersectionForNonduplicateElements(t *testing.T) {
+	var a = []int{2,4,6,8,10,12}
+	var b = []int{3,6,9,12,15}
+	got := intersection(a, b)
+	want := []int {12,6}
+	assertArrayEqual(got, want, t)
+}
+
+func TestIntersectionForWithduplicateElements(t *testing.T) {
+	var a = []int{2,4,6,8,10,12,8,4,0}
+	var b = []int{3,6,9,12,15,9,1}
+	got := intersection(a, b)
+	want := []int {6,12}
+	assertArrayEqual(got, want, t)
+}
+
+
+func assertArrayEqual(got []int,want []int, t *testing.T){
+	sort.Ints(got)
+	sort.Ints(want)
+	if (!reflect.DeepEqual(got,want) ) {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
